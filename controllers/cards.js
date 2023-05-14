705bc167199a1279441c697119ac0a060bf01159
Denis Sharikadze <denissharikadze94@yandex.ru>
2023-05-14 13:26:25 +0400

    feat: add custom errors

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,39 +1,57 @@
 const Card = require('../models/card');
+const { checkAviability } = require('../utils/utils');
+
+const handleError = (err, res) => {
+  console.log(err.name)
+  if(err.name === 'ValidationError') {
+    res.status(400).send({ message: 'Переданы некорректные данные при создании карточки.' })
+    return;
+  }
+  if(err.name === 'NotFoundError' || err.name === 'CastError') {
+    res.status(404).send({ message: 'Карточка с указанным _id не найдена.' });
+    return;
+  }
+  if(err.name === 'LikeError') {
+    res.status(400).send({ message: 'Переданы некорректные данные для постановки/снятии лайка.' });
+    return;
+  }
+  res.status(500).send({ message: `${err.name}: ${err.message}` });
+}
 
 const getCards = (req, res) => {
   Card.find({})
     .then(cards => res.send(cards))
-    .catch(err => res.status(500).send({ message: 'Server error' }))
+    .catch(err => handleError(err, res))
 }
 
 const createCard = (req, res) => {
   const { name, link } = req.body;
   const owner = req.user._id
   Card.create({ name, link, owner })
-    .then(card => res.status(201).send(card))
-    .catch(err => res.status(400).send({ message: 'Error: invalid data' }))
+    .then(card => checkAviability(card, res))
+    .catch(err => handleError(err, res))
 }
 
 const deleteCard = (req, res) => {
   const { cardId } = req.params;
   Card.findByIdAndRemove(cardId)
     .then(card => res.send(card))
-    .catch(err => res.status(404).send({ message: 'Error: card not found' }))
+    .catch(err => handleError(err, res))
 }
 
 const addLike = (req, res) => {
   const { cardId } = req.params;
   const userId = req.user._id;
   Card.findByIdAndUpdate(cardId, { $addToSet: { likes: userId } }, { new: true })
-    .then(card => res.send(card))
-    .catch(err => res.status(500).send({ message: 'Server error' }))
+    .then(card => checkAviability(card, res))
+    .catch(err => handleError(err, res))
 }
 const removeLike = (req, res) => {
   const { cardId } = req.params;
   const userId = req.user._id;
   Card.findByIdAndUpdate(cardId, { $pull: { likes: userId } }, { new: true })
-    .then(card => res.send(card))
-    .catch(err => res.status(500).send({ message: 'Server error' }))
+    .then(card => checkAviability(card, res))
+    .catch(err => andleError(err, res))
 }
 
 module.exports = {
--- controllers/users.js
+++ controllers/users.js
@@ -1,39 +1,52 @@
 const User = require('../models/user');
+const { checkAviability } = require('../utils/utils');
+
+const handleError = (err, res) => {
+  if(err.name === 'ValidationError') {
+    res.status(400).send({ message: 'Переданы некорректные данные при обновлении профиля.' })
+    return;
+  }
+  if(err.name === 'NotFoundError' || err.name === 'CastError') {
+    res.status(404).send({ message: 'Пользователь по указанному _id не найден.' });
+    return;
+  }
+  res.status(500).send({ message: `${err.name}: ${err.message}` });
+}
 
 const getUsers = (req, res) => {
   User.find({})
     .then(users => res.send(users))
-    .catch(err => res.status(500).send({ message: 'Server error' }))
-}
-
-const getUserById = (req, res) => {
-  const { userId } = req.params
-  User.findById(userId)
-    .then(user => res.send(user))
-    .catch(err => res.status(404).send({ message: 'Error: User not found' }))
+    .catch(err => handleError(err, res))
 }
 
 const createUser = (req, res) => {
   const userData = req.body;
   User.create(userData)
     .then(user => res.status(201).send(user))
-    .catch(err => res.status(400).send({ message: 'Error: invalid data' }))
+    .catch(err => handleError(err, res))
+}
+
+const getUserById = (req, res) => {
+  const { userId } = req.params
+  User.findById(userId)
+    .then(user => checkAviability(user, res))
+    .catch(err => handleError(err, res))
 }
 
 const updateProfile = (req, res) => {
   const userData = req.body;
   const userId = req.user._id;
-  User.findByIdAndUpdate(userId, userData, { new: true })
-    .then(user => res.send(user))
-    .catch(err => res.status(500).send({ message: 'Server error' }))
+  User.findByIdAndUpdate(userId, userData, { new: true, runValidators: true })
+    .then(user => checkAviability(user, res))
+    .catch(err => handleError(err, res))
 }
 
 const updateAvatar = (req, res) => {
   const { avatar } = req.body;
   const userId = req.user._id;
   User.findByIdAndUpdate(userId, { avatar }, { new: true })
-    .then(user => res.send(user))
-    .catch(err => res.status(500).send({ message: 'Server error' }))
+    .then(user => checkAviability(user, res))
+    .catch(err => handleError(err, res))
 }
 
 module.exports = {
